3ac4a3574b075dfeea6a7209b1202f9a85259547
ditole <tolerevo@hotmail.com>
2023-11-25 14:23:42 -0600

    add inline styles to bussiness component

--- src/componentes/Bussines.js
+++ src/componentes/Bussines.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import '../hojas-de-estilo/Bussiness.css';
 
+const styles = {
+  fontSize:'1em',
+  color: 'goldenrod',
+  listStyle: 'none',
+  fontWeight: '700',
+  textAlign: 'right'
+};
 
 const bussiness = {
   ImageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
@@ -21,9 +28,9 @@ function Bussiness() {
       <h1>{bussiness.name}</h1>
       <img src={bussiness.ImageSrc} alt={bussiness.name} />
       <ul>
-        <li>{bussiness.address}.  .  .  .   {bussiness.category}</li>
-        <li>{bussiness.city}.  .  .  . .  .  .  . .  .   Rating{bussiness.rating}</li>
-        <li>state {bussiness.state} .  .  .   .  .  .  .  .  .  .  .  Review{bussiness.reviewCount}</li>
+        <li>{bussiness.address}<hr /><h2 style={styles}>{bussiness.category}</h2></li>
+        <li>{bussiness.city}<hr /><h2 style={styles}> Rating{bussiness.rating}</h2> </li>
+        <li>state {bussiness.state}<hr /><h2 style={styles}> Review{bussiness.reviewCount}</h2></li>
         <li>zipCode {bussiness.zipCode}</li>
        </ul>
      </div>
