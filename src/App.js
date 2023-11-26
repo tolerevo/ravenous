03a5f62be1fc34539d9c93670762279ee4f439af
ditole <tolerevo@hotmail.com>
2023-11-26 16:34:05 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import BussinessList from './componentes/BussinessList';
+import SearchBar from './componentes/SearchBar';
 
 function App() {
   return (
     <div className='contenedor-principal'>
+      <nav>
+        <SearchBar />
+      </nav>
       <main className='contenedor-componente '>
         <BussinessList className='contenedor-restaurante' />
         <BussinessList className='contenedor-restaurante' />
--- src/componentes/SearchBar.js
+++ src/componentes/SearchBar.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function SearchBar(){
-
+function SearchBar(props){
+  function handleClick() {
+    let submit = 'loading your search...';
+    alert(submit);
+  };
+return (
+  <nav>
+    <input type='search' onChange={'handleChange'} placeholder=' what?'/>
+    <input placeholder=' where?'/>
+    <button onClick={handleClick}>Let's Go!</button>
+  </nav>
+);
 }
 
 export default SearchBar;
